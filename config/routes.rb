8037d4475e782b707f9bd889bffde9a076f5f23f
Mostafa Sakr <msakr3@gmail.com>
2023-06-09 16:46:56 -0600

    Refactor routes and add admin namespace.

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get '/enrollments/new', to: 'enrollments#new'
-  get '/enrollments', to: 'enrollments#index'
-  get '/enrollments/:id/edit', to: 'enrollments#edit'
-  patch '/enrollments/:id', to: 'enrollments#update'
-  post '/enrollments', to: 'enrollments#create'
+  namespace :admin do
+    resources :enrollments do
+      resources :students 
+    end
+  end
 
-  get '/enrollments/:id/students/new', to: 'students#new'
-  post '/enrollments/:enrollment_id/students', to: 'students#create'
-  get '/enrollments/:enrollment_id/students', to: 'students#index'
-  get '/enrollments/:enrollment_id/students/:id', to: 'students#show'
-  get '/enrollments/:enrollment_id/students/:id/edit', to: 'students#edit'
-  delete '/enrollments/:enrollment_id/students/:id', to: 'students#delete'
-  patch '/enrollments/:enrollment_id/students/:id', to: 'students#update'
+  resources :enrollments, only: [:index, :show] do
+    resources :students, only: [:new, :create]
+  end
 end
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,8 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
-// import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [
-    RubyPlugin(),
-  ],
-  // build: {
-  //   // generate manifest.json in outDir
-  //   manifest: true,
-  //   rollupOptions: {
-  //     // overwrite default .html entry
-  //     input: '~/app/frontend/components/index.jsx',
-  //   },
-  // },
+    RubyPlugin()
+  ]
 })
