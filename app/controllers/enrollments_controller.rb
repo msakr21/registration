241671771ddf083ef558b8adc2cd5b0e142835f7
Mostafa Sakr <msakr3@gmail.com>
2023-06-09 23:21:00 -0600

    remove create method from non admin enrollments controller.

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -5,9 +5,4 @@ class EnrollmentsController < ApplicationController
     end
     @enrollments = enrollments_array.to_json
   end
-
-  def create
-    Enrollment.create(location: params[:location], schedule: params[:schedule], student_limit: params[:student_limit])
-    redirect_to '/enrollments'
-  end
 end
