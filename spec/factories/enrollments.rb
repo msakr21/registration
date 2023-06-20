7a591f2b206e57e9141f2305290bc7a5deb55553
bkeener <bkeener7@gmail.com>
2023-06-20 11:03:39 -0600

    Update FactoryBot

--- spec/factories/enrollments.rb
+++ spec/factories/enrollments.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :enrollment do
-    location { Faker::Address.city }
+    location { ['Eloise May', 'Smoky Hill', 'Sheridan'].sample }
     schedule { Faker::Date.forward(days: 3000) }
     student_limit { 30 }
   end
