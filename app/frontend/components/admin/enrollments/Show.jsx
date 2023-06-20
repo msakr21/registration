31f180c59a85270692b30c3ee7df14e80a72c200
bkeener <bkeener7@gmail.com>
2023-06-20 10:15:07 -0600

    Admin Show WIP

--- app/frontend/components/admin/enrollments/Show.jsx
+++ app/frontend/components/admin/enrollments/Show.jsx
@@ -1,40 +1,37 @@
 import React from 'react';
 import { Button } from '@mui/material';
 
+function isAtCapacity(limit, number) {
+  return limit <= number;
+};
+
+function URISetter(main, id, sub, page) {
+  return `/${main}/${id}/${sub}${page}`;
+}
+
 function AdminEnrollmentShow() {
   const enrollments = JSON.parse(document.getElementById("data").getAttribute("enrollments"));
-  
-  function AtCapacity(limit,number) {
-    if (limit > number) {
-      return false;
-    } else {
-      return true;
-    }
-  };
 
-  function URISetter(main,id,sub,page) {
-    return `/${main}/${id}/${sub}${page}`
-  }
+  const listEnrollments = enrollments.map(({ id, location, schedule, student_limit, students }) => (
+    <div key={id}>
+      <p>Location: {location}</p>
+      <p>Schedule: {schedule}</p>
+      <p>Student Limit: {student_limit}</p>
+      <p>Number of Students: {students}</p>
+      <Button disabled={isAtCapacity(student_limit, students)} href={URISetter("enrollments", id, "students/", "new")}>Register new student for this session</Button>
+      <Button href={URISetter("admin/enrollments", id, "edit", "")}>Edit Session</Button>
+      <Button href={URISetter("admin/enrollments", id, "", "")}>Show Session Details</Button>
+      <Button href={URISetter("admin/enrollments", id, "delete", "")}>Delete Session</Button>
+    </div>
+  ));
 
-  const listEnrollments = enrollments.map((enrollment) => 
-  <div key={enrollment.id}>
-  <p>Location: {enrollment.location}</p>
-  <p>Schedule: {enrollment.schedule}</p>
-  <p>Student Limit: {enrollment.student_limit}</p>
-  <p>Number of Students: {enrollment.students}</p>
-  <Button disabled={AtCapacity(enrollment.student_limit,enrollment.students)} href={URISetter("enrollments",enrollment.id,"students/","new")}>Register new student for this session</Button>
-  <Button href={URISetter("admin/enrollments",enrollment.id,"edit","")}>Edit Session</Button>
-  <Button href={URISetter("admin/enrollments",enrollment.id,"","")}>Show Session Details</Button>
-  <Button href={URISetter("admin/enrollments",enrollment.id,"delete","")}>Delete Session</Button>
-  </div>
+  return (
+    <div>
+      <Button href={"/admin/enrollments/new"}>New Enrollment Session</Button>
+      <h3>Available Enrollment Sessions:</h3>
+      {listEnrollments}
+    </div>
   );
-
-  return <div>
-          <a href={"/admin/enrollments/new"}>New Enrollment Session</a>
-          <br></br>
-          <h3>Available Enrollment Sessions:</h3>
-           {listEnrollments}
-         </div>
 }
 
 export default AdminEnrollmentShow;
