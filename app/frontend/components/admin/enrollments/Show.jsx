4eead4f325f9d234b4e11a826ab8899e30c99586
bkeener <bkeener7@gmail.com>
2023-06-21 16:52:11 -0600

    Format enrollments show page

--- app/frontend/components/admin/enrollments/Show.jsx
+++ app/frontend/components/admin/enrollments/Show.jsx
@@ -4,7 +4,7 @@ import { Button, Card, Col, Row, Alert, Table } from "react-bootstrap";
 import DeleteConfirmation from '~/components/DeleteConfirmation.jsx';
 import { Trash, Pencil } from "react-bootstrap-icons";
 
-function URISetter(main,id,sub,page) {
+function URISetter(main, id, sub, page) {
   return `/${main}/${id}/${sub}${page}`;
 }
 
@@ -12,55 +12,60 @@ function AdminEnrollmentShow() {
   const enrollment = JSON.parse(document.getElementById("data").getAttribute("enrollment"));
   const students = JSON.parse(document.getElementById("data").getAttribute("students"));
 
-  const showEnrollment = (enrollment, students)=> (
+  const showEnrollment = (enrollment, students) => (
     <Row>
-      <Card className="enrollment-card card mx-auto" bg="light" text="dark" border="dark" style={{ width: "40%", height: "50%"}}>
-        <Card.Header style={{ textAlign: "center" }}><a href={"/admin/enrollments"}>Enrollment Index</a> &emsp; <a href={"/admin/enrollments/new"}>New Enrollment Session</a></Card.Header>
-          <Card.Body style={{ textAlign: "center" }}>
-            <Card.Title>{enrollment.location} — {enrollment.date}</Card.Title>
-            <Card.Subtitle>{enrollment.time}</Card.Subtitle><br />
-            <Card.Text>Student Limit: {enrollment.student_limit}</Card.Text>
-            <Card.Text>Number of Students: {students.length}</Card.Text>
-            <Row xs={2} md={2} >
-              <Button href={URISetter("admin/enrollments", enrollment.id, "edit", "")} size="sm" variant="outline-dark">Edit Session</Button>
-              <Button size="sm" variant="outline-dark" onClick={() => ShowDeleteModal(enrollment, enrollment.id)}>Delete Session</Button>
-            </Row>
-          </Card.Body>
+      <Card className="enrollment-card card mx-auto" bg="light" text="dark" border="dark" style={{ width: "40%", height: "50%" }}>
+        <Card.Header style={{ textAlign: "center" }}>
+          <a href={"/admin/enrollments"}>Enrollment Index</a> &emsp;
+          <a href={"/admin/enrollments/new"}>New Enrollment Session</a>
+        </Card.Header>
+        <Card.Body style={{ textAlign: "center" }}>
+          <Card.Title>{enrollment.location} — {enrollment.date}</Card.Title>
+          <Card.Subtitle>{enrollment.time}</Card.Subtitle>
+          <br />
+          <Card.Text>Student Limit: {enrollment.student_limit}</Card.Text>
+          <Card.Text>Number of Students: {students.length}</Card.Text>
+          <Row xs={2} md={2}>
+            <Button href={URISetter("admin/enrollments", enrollment.id, "edit", "")} size="sm" variant="outline-dark">Edit Session</Button>
+            <Button size="sm" variant="outline-dark" onClick={() => ShowDeleteModal(enrollment, enrollment.id)}>Delete Session</Button>
+          </Row>
+        </Card.Body>
       </Card>
     </Row>
   );
 
-  const enrolledStudents = (students) => {
-    return (
-      <div>
-        <h4>Enrolled Students:</h4>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Email</th>
-              <th>Phone</th>
-              <th>Actions</th>
+  const enrolledStudents = (students) => (
+    <div>
+      <h4>Enrolled Students:</h4>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {students.map((student, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{student.first_name}</td>
+              <td>{student.last_name}</td>
+              <td>{student.email}</td>
+              <td>{student.phone}</td>
+              <td>
+                <Pencil color="blue" /> &emsp;
+                <Trash color="red" />
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {students.map((student, index) => (
-              <tr> 
-                <td>{index + 1}</td>
-                <td>{student.first_name}</td>
-                <td>{student.last_name}</td>
-                <td>{student.email}</td>
-                <td>{student.phone}</td>
-                <td><Pencil color="blue"></Pencil> &emsp; <Trash color="red"></Trash></td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-      </div>
-    );
-  }
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
 
   return (
     <div style={{ height: "95vh" }}>
