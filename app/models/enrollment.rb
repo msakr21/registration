f2a810b5e580d40e48c2b52a766677d1bc9b581f
bkeener <bkeener7@gmail.com>
2023-06-21 17:04:00 -0600

    Refactor models

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -6,10 +6,10 @@ class Enrollment < ApplicationRecord
       {
         id: enrollment.id,
         location: enrollment.location,
-        date: enrollment.schedule.in_time_zone('Mountain Time (US & Canada)').strftime('%m/%d/%Y'),
-        time: enrollment.schedule.in_time_zone('Mountain Time (US & Canada)').strftime('%I:%M %p'),
+        date: enrollment.formatted_date,
+        time: enrollment.formatted_time,
         student_limit: enrollment.student_limit,
-        students: enrollment.students.length
+        students: enrollment.students.count
       }
     end
 
@@ -18,13 +18,22 @@ class Enrollment < ApplicationRecord
 
   def self.enrollment_detail(enrollment_id)
     enrollment = find(enrollment_id.to_i)
+
     {
       id: enrollment.id,
       location: enrollment.location,
-      date: enrollment.schedule.in_time_zone('Mountain Time (US & Canada)').strftime('%m/%d/%Y'),
-      time: enrollment.schedule.in_time_zone('Mountain Time (US & Canada)').strftime('%I:%M %p'),
+      date: enrollment.formatted_date,
+      time: enrollment.formatted_time,
       student_limit: enrollment.student_limit,
-      students: enrollment.students.length
+      students: enrollment.students.count
     }.to_json
+  end
+
+  def formatted_date
+    schedule.in_time_zone('Mountain Time (US & Canada)').strftime('%m/%d/%Y')
+  end
+
+  def formatted_time
+    schedule.in_time_zone('Mountain Time (US & Canada)').strftime('%I:%M %p')
   end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,13 +2,7 @@ class Student < ApplicationRecord
   belongs_to :enrollment
 
   def self.list_student_data
-    all.map do |student|
-      {
-        first_name: student.first_name,
-        last_name: student.last_name,
-        email: student.email,
-        phone: student.phone
-      }
-    end.to_json
+    students = all.select(:first_name, :last_name, :email, :phone)
+    students.to_json
   end
 end
