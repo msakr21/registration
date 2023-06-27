ced6069a7f89ab4e99d685c6d70d22ee1cd39bc9
bkeener <bkeener7@gmail.com>
2023-06-27 11:11:53 -0600

    Refactor students controller

--- app/controllers/admin/students_controller.rb
+++ app/controllers/admin/students_controller.rb
@@ -1,28 +1,45 @@
 class Admin::StudentsController < ApplicationController
+  before_action :set_enrollment, only: [:create]
+  before_action :set_student, only: [:update, :destroy]
+
   def new
     @enrollment_id = params[:enrollment_id]
   end
 
   def create
-    enrollment = Enrollment.find(params[:enrollment_id])
-    enrollment.students.create!(
-      first_name: params[:first_name],
-      last_name: params[:last_name],
-      email: params[:email],
-      phone: params[:phone],
-      language: params[:language]
-    )
-    redirect_to '/admin/enrollments'
+    create_student
+    redirect_to admin_enrollments_path
   end
 
   def update
-    student = Student.find(params[:id])
-    student.update!(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], phone: params[:phone], language: params[:language])
-    redirect_to "/admin/enrollments/#{params[:enrollment_id]}"
+    update_student
+    redirect_to admin_enrollment_path(@student.enrollment_id)
   end
 
   def destroy
-    Student.destroy(params[:id])
-    redirect_to controller: 'admin/enrollments', action: 'show', id: params[:enrollment_id]
+    @student.destroy
+    redirect_to admin_enrollment_path(@student.enrollment_id)
+  end
+
+  private
+
+  def set_enrollment
+    @enrollment = Enrollment.find(params[:enrollment_id])
+  end
+
+  def set_student
+    @student = Student.find(params[:id])
+  end
+
+  def student_params
+    params.permit(:first_name, :last_name, :email, :phone, :language)
+  end
+
+  def create_student
+    @enrollment.students.create!(student_params)
+  end
+
+  def update_student
+    @student.update!(student_params)
   end
 end
