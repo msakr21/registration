99fad086719616d90e73aaae8f486d0717f4edbc
Mostafa Sakr <msakr3@gmail.com>
2023-07-08 14:05:15 -0600

    Change the enrollment index pages to have sections for each library, add a model method that queries enrollments by location, center cards on page, update tests.

--- app/frontend/components/EnrollmentIndex.jsx
+++ app/frontend/components/EnrollmentIndex.jsx
@@ -5,7 +5,9 @@ import DeleteConfirmation from '~/components/DeleteConfirmation.jsx';
 
 function EnrollmentIndex(props) {
   const csrf_token = document.head.getElementsByTagName('meta')[2].content;
-  const enrollments = JSON.parse(document.getElementById("data").getAttribute("enrollments"));
+  const mayEnrollments = JSON.parse(document.getElementById("data").getAttribute("may_enrollments"));
+  const sheridanEnrollments = JSON.parse(document.getElementById("data").getAttribute("sheridan_enrollments"));
+  const smokyEnrollments = JSON.parse(document.getElementById("data").getAttribute("smoky_enrollments"));
   const newStudentError = document.getElementById("data").getAttribute("new_student_error");
   const [displayConfirmationModal, setDisplayConfirmationModal] = useState(false);
   const [deleteMessage, setDeleteMessage] = useState(null);
@@ -115,27 +117,74 @@ function EnrollmentIndex(props) {
     }
   }
 
-  const listEnrollments = enrollments.map((enrollment) =>
-    <Col key={enrollment.id}>
-      <Card className="enrollment-card" bg="light" text="dark" border="dark">
-        <Card.Body style={{ textAlign: "center" }}>
-          <Card.Title>{enrollment.location}</Card.Title>
-          <Card.Subtitle>{enrollment.date}<br />{enrollment.time} — {endTime(enrollment.time)}</Card.Subtitle>
-          <br />
-          {UserListUI(props.admin, enrollment)}
-        </Card.Body>
-      </Card>
-    </Col>
-  );
+  // const listEnrollments = enrollments.map((enrollment) =>
+  //   <Col key={enrollment.id}>
+  //     <Card className="enrollment-card" bg="light" text="dark" border="dark">
+  //       <Card.Body style={{ textAlign: "center" }}>
+  //         <Card.Title>{enrollment.location}</Card.Title>
+  //         <Card.Subtitle>{enrollment.date}<br />{enrollment.time} — {endTime(enrollment.time)}</Card.Subtitle>
+  //         <br />
+  //         {UserListUI(props.admin, enrollment)}
+  //       </Card.Body>
+  //     </Card>
+  //   </Col>
+  // );
+
+  const listMayEnrollments = mayEnrollments.map((enrollment) =>
+  <Col key={enrollment.id}>
+    <Card className="enrollment-card" bg="light" text="dark" border="dark">
+      <Card.Body style={{ textAlign: "center" }}>
+        <Card.Title>{enrollment.location}</Card.Title>
+        <Card.Subtitle>{enrollment.date}<br />{enrollment.time} — {endTime(enrollment.time)}</Card.Subtitle>
+        <br />
+        {UserListUI(props.admin, enrollment)}
+      </Card.Body>
+    </Card>
+  </Col>
+);
+
+const listSheridanEnrollments = sheridanEnrollments.map((enrollment) =>
+<Col key={enrollment.id}>
+  <Card className="enrollment-card" bg="light" text="dark" border="dark">
+    <Card.Body style={{ textAlign: "center" }}>
+      <Card.Title>{enrollment.location}</Card.Title>
+      <Card.Subtitle>{enrollment.date}<br />{enrollment.time} — {endTime(enrollment.time)}</Card.Subtitle>
+      <br />
+      {UserListUI(props.admin, enrollment)}
+    </Card.Body>
+  </Card>
+</Col>
+);
+
+const listSmokyEnrollments = smokyEnrollments.map((enrollment) =>
+<Col key={enrollment.id}>
+  <Card className="enrollment-card" bg="light" text="dark" border="dark">
+    <Card.Body style={{ textAlign: "center" }}>
+      <Card.Title>{enrollment.location}</Card.Title>
+      <Card.Subtitle>{enrollment.date}<br />{enrollment.time} — {endTime(enrollment.time)}</Card.Subtitle>
+      <br />
+      {UserListUI(props.admin, enrollment)}
+    </Card.Body>
+  </Card>
+</Col>
+);
 
   return (
     <Card border="light">
       {DisplayDeleteSuccess()}
       {NewStudentError()}
       {UserHeaderUI(props.admin)}
-      <Card.Title style={{ textAlign: "center", margin: "2%" }}>Available Enrollment Sessions:</Card.Title>
-      <Row xs={3} md={3} className="g-4">
-        {listEnrollments}
+      <Card.Title style={{ textAlign: "center", margin: "2%" }}>Eloise May Enrollment Sessions:</Card.Title>
+      <Row xs={2} md={3} className="g-4 justify-content-center">
+        {listMayEnrollments}
+      </Row>
+      <Card.Title style={{ textAlign: "center", margin: "2%" }}>Sheridan Enrollment Sessions:</Card.Title>
+      <Row xs={2} md={3} className="g-4 justify-content-center">
+        {listSheridanEnrollments}
+      </Row>
+      <Card.Title style={{ textAlign: "center", margin: "2%" }}>Smoky Hill Enrollment Sessions:</Card.Title>
+      <Row xs={2} md={3} className="g-4 justify-content-center">
+        {listSmokyEnrollments}
       </Row>
       <DeleteConfirmation showModal={displayConfirmationModal} confirmModal={submitDelete} hideModal={hideConfirmationModal} path={deletePath} message={deleteMessage} authenticity={csrf_token} />
     </Card>
