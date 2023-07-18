6ef49db08d139f9bb47bb2628bda2d63870131e5
Mostafa Sakr <msakr3@gmail.com>
2023-07-17 22:27:58 -0600

    Further refactor Show.jsx by turing the enrolledStudents function into its own component in a separate file.

--- app/frontend/components/Show.jsx
+++ app/frontend/components/Show.jsx
@@ -1,9 +1,7 @@
 import React, { useState } from 'react';
-import { Table } from "react-bootstrap";
 import DeleteConfirmation from '~/components/DeleteConfirmation.jsx';
-import TableEditForm from '~/components/TableEditForm.jsx';
-import TableRows from '~/components/TableRows.jsx';
 import ShowEnrollmentCard from '~/components/ShowEnrollmentCard.jsx';
+import EnrolledStudents from '~/components/EnrolledStudents.jsx'; 
 
 function AdminEnrollmentShow() {
   const csrf_token = document.head.getElementsByTagName('meta')[2].content;
@@ -42,35 +40,11 @@ function AdminEnrollmentShow() {
     setDisplayConfirmationModal(false);
   };
 
-  const enrolledStudents = (students) => (
-    <div>
-      {TableEditForm(enrollment, formValue, csrf_token)}
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th className="text-center" width="4%" style={{ whiteSpace: 'normal', wordWrap: 'break-word' }}>#</th>
-            <th className="text-center" width="16.8%">First Name</th>
-            <th className="text-center" width="22.8%">Last Name</th>
-            <th className="text-center" width="23.8%">Email</th>
-            <th className="text-center" width="10%">Phone</th>
-            <th className="text-center" width="11%">Language</th>
-            <th className="text-center" width="12.6%" style={{ whiteSpace: 'normal', wordWrap: 'break-word' }}>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {students.map((student, index) => (
-            TableRows(rowEdit, formValue, student, index, handleChange, enrollment, setRowEdit, setFormValue, setDeletePath, setDeleteMessage, setDisplayConfirmationModal)
-          ))}
-        </tbody>
-      </Table>
-    </div>
-  );
-
   return (
     <div style={{ height: "95vh" }}>
       <div id='printable' style={{ height: "95vh" }}>
         {ShowEnrollmentCard(enrollment, students, setDeletePath, setDeleteMessage, setDisplayConfirmationModal)}
-        {enrolledStudents(students)}
+        {EnrolledStudents(students, rowEdit, enrollment, formValue, csrf_token, handleChange, setRowEdit, setFormValue, setDeletePath, setDeleteMessage, setDisplayConfirmationModal)}
       </div>
       <DeleteConfirmation showModal={displayConfirmationModal} confirmModal={submitDelete} hideModal={hideConfirmationModal} path={deletePath} message={deleteMessage} authenticity={csrf_token} />
     </div>
