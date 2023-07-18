629b23614ddebd8a276f193ab0a07133d944981e
Mostafa Sakr <msakr3@gmail.com>
2023-07-17 22:41:49 -0600

    Rename Show.jsx to match function name.

--- app/frontend/entrypoints/admin_enrollment_show.jsx
+++ app/frontend/entrypoints/admin_enrollment_show.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import AdminEnrollmentShow from '~/components/Show';
+import AdminEnrollmentShow from '~/components/AdminEnrollmentShow';
 
 const container = document.getElementById('show');
 const root = ReactDOM.createRoot(container);
