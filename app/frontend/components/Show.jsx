f1ec208bd16971619ab31724267c6f2b0efb4052
Mostafa Sakr <msakr3@gmail.com>
2023-07-17 22:21:02 -0600

    Further refactor Show.jsx by turing the showEnrollment function into its own component in a separate file.

--- app/frontend/components/Show.jsx
+++ app/frontend/components/Show.jsx
@@ -1,13 +1,9 @@
 import React, { useState } from 'react';
-import { Button, Card, Row, Table } from "react-bootstrap";
+import { Table } from "react-bootstrap";
 import DeleteConfirmation from '~/components/DeleteConfirmation.jsx';
-import endTime from '~/components/EndTime.js'
-import showDeleteModal from '~/components/showDeleteModal.js';
 import TableEditForm from '~/components/TableEditForm.jsx';
 import TableRows from '~/components/TableRows.jsx';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPrint } from "@fortawesome/free-solid-svg-icons";
-import Print from 'print-js';
+import ShowEnrollmentCard from '~/components/ShowEnrollmentCard.jsx';
 
 function AdminEnrollmentShow() {
   const csrf_token = document.head.getElementsByTagName('meta')[2].content;
@@ -46,29 +42,6 @@ function AdminEnrollmentShow() {
     setDisplayConfirmationModal(false);
   };
 
-  const showEnrollment = (enrollment, students) => (
-    <Row>
-      <Card id="enrollmentCard" className="enrollment-card card mx-auto px-0" bg="light" text="dark" border="dark" style={{ width: "40%", height: "50%" }}>
-        <Card.Header style={{ textAlign: "center" }}>
-          <a href={"/admin/enrollments"}>Enrollment Index</a> &nbsp;&nbsp;
-          <a href={"/admin/enrollments/new"}>New Enrollment Session</a> &nbsp;&nbsp;
-          <Button name="print" style={{ outline: "none", border: "0", boxShadow: "none", backgroundColor: "transparent" }} onClick={() => {Print({ printable: 'printable', type: 'html', scanStyles: false, css: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css'})}}> <FontAwesomeIcon icon={faPrint} color="grey" /> </Button>
-        </Card.Header>
-        <Card.Body style={{ textAlign: "center" }}>
-          <Card.Title>{enrollment.location} — {enrollment.date}</Card.Title>
-          <Card.Subtitle>{enrollment.time} — {endTime(enrollment.time)}</Card.Subtitle>
-          <br />
-          <Card.Text>Student Limit: {enrollment.student_limit}</Card.Text>
-          <Card.Text>Number of Students: {students.length}</Card.Text>
-          <Row xs={2} md={2}>
-            <Button href={`/admin/enrollments/${enrollment.id}/edit`} size="sm" variant="outline-dark">Edit Session</Button>
-            <Button size="sm" variant="outline-dark" onClick={() => showDeleteModal(enrollment, enrollment.id, setDeletePath, setDeleteMessage, setDisplayConfirmationModal)}>Delete Session</Button>
-          </Row>
-        </Card.Body>
-      </Card>
-    </Row>
-  );
-
   const enrolledStudents = (students) => (
     <div>
       {TableEditForm(enrollment, formValue, csrf_token)}
@@ -96,12 +69,12 @@ function AdminEnrollmentShow() {
   return (
     <div style={{ height: "95vh" }}>
       <div id='printable' style={{ height: "95vh" }}>
-        {showEnrollment(enrollment, students)}
+        {ShowEnrollmentCard(enrollment, students, setDeletePath, setDeleteMessage, setDisplayConfirmationModal)}
         {enrolledStudents(students)}
       </div>
       <DeleteConfirmation showModal={displayConfirmationModal} confirmModal={submitDelete} hideModal={hideConfirmationModal} path={deletePath} message={deleteMessage} authenticity={csrf_token} />
     </div>
   );
-}
+};
 
 export default AdminEnrollmentShow;
---
+++ app/frontend/components/ShowEnrollmentCard.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Button, Card, Row } from "react-bootstrap";
+import endTime from '~/components/EndTime.js'
+import showDeleteModal from '~/components/showDeleteModal.js';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPrint } from "@fortawesome/free-solid-svg-icons";
+import Print from 'print-js';
+
+const ShowEnrollmentCard = (enrollment, students, setDeletePath, setDeleteMessage, setDisplayConfirmationModal) => (
+  <Row>
+    <Card id="enrollmentCard" className="enrollment-card card mx-auto px-0" bg="light" text="dark" border="dark" style={{ width: "40%", height: "50%" }}>
+      <Card.Header style={{ textAlign: "center" }}>
+        <a href={"/admin/enrollments"}>Enrollment Index</a> &nbsp;&nbsp;
+        <a href={"/admin/enrollments/new"}>New Enrollment Session</a> &nbsp;&nbsp;
+        <Button name="print" style={{ outline: "none", border: "0", boxShadow: "none", backgroundColor: "transparent" }} onClick={() => {Print({ printable: 'printable', type: 'html', scanStyles: false, css: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css'})}}> <FontAwesomeIcon icon={faPrint} color="grey" /> </Button>
+      </Card.Header>
+      <Card.Body style={{ textAlign: "center" }}>
+        <Card.Title>{enrollment.location} — {enrollment.date}</Card.Title>
+        <Card.Subtitle>{enrollment.time} — {endTime(enrollment.time)}</Card.Subtitle>
+        <br />
+        <Card.Text>Student Limit: {enrollment.student_limit}</Card.Text>
+        <Card.Text>Number of Students: {students.length}</Card.Text>
+        <Row xs={2} md={2}>
+          <Button href={`/admin/enrollments/${enrollment.id}/edit`} size="sm" variant="outline-dark">Edit Session</Button>
+          <Button size="sm" variant="outline-dark" onClick={() => showDeleteModal(enrollment, enrollment.id, setDeletePath, setDeleteMessage, setDisplayConfirmationModal)}>Delete Session</Button>
+        </Row>
+      </Card.Body>
+    </Card>
+  </Row>
+);
+
+export default ShowEnrollmentCard;
