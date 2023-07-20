0e132d5345291d45a87934f8e916ab72d101cdcc
bkeener <bkeener7@gmail.com>
2023-07-20 10:46:50 -0600

    Remove unneeded comments and organize config files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,60 +3,43 @@ require 'active_support/core_ext/integer/time'
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # In the development environment your application's code is reloaded any time
-  # it changes. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
+  # Development environment settings
+  # These settings are perfect for development since you don't have to restart the web server when you make code changes.
+  config.cache_classes = false  # Do not cache classes, code is reloaded any time it changes.
+  config.eager_load = false     # Do not eager load code on boot.
+  config.consider_all_requests_local = true # Show full error reports.
+  config.server_timing = true # Enable server timing.
 
-  # Do not eager load code on boot.
-  config.eager_load = false
-
-  # Show full error reports.
-  config.consider_all_requests_local = true
-
-  # Enable server timing
-  config.server_timing = true
-
-  # Enable/disable caching. By default caching is disabled.
-  # Run rails dev:cache to toggle caching.
+  # Cache configuration settings
+  # By default caching is disabled. Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
-
     config.cache_store = :memory_store
     config.public_file_server.headers = {
       'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
-
     config.cache_store = :null_store
   end
 
-  # Print deprecation notices to the Rails logger.
-  config.active_support.deprecation = :log
+  # Active support configuration settings
+  config.active_support.deprecation = :log # Print deprecation notices to the Rails logger.
+  config.active_support.disallowed_deprecation = :raise # Raise exceptions for disallowed deprecations.
+  config.active_support.disallowed_deprecation_warnings = [] # Specify which deprecation messages to disallow.
 
-  # Raise exceptions for disallowed deprecations.
-  config.active_support.disallowed_deprecation = :raise
+  # Active Record configuration settings
+  config.active_record.migration_error = :page_load # Raise an error on page load if there are pending migrations.
+  config.active_record.verbose_query_logs = true # Highlight code that triggered database queries in logs.
 
-  # Tell Active Support which deprecation messages to disallow.
-  config.active_support.disallowed_deprecation_warnings = []
-
-  # Raise an error on page load if there are pending migrations.
-  config.active_record.migration_error = :page_load
-
-  # Highlight code that triggered database queries in logs.
-  config.active_record.verbose_query_logs = true
-
-  # Suppress logger output for asset requests.
-  config.assets.quiet = true
-
-  # Raises error for missing translations.
-  # config.i18n.raise_on_missing_translations = true
-
-  # Annotate rendered view with file names.
-  # config.action_view.annotate_rendered_view_with_filenames = true
+  # Asset configuration settings
+  config.assets.quiet = true # Suppress logger output for asset requests.
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Uncomment the lines below for additional configuration options
+  # config.i18n.raise_on_missing_translations = true  # Raises error for missing translations.
+  # config.action_view.annotate_rendered_view_with_filenames = true  # Annotate rendered view with file names.
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -7,44 +7,37 @@ require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.assets.compile = true
-  # Turn false under Spring and add config.action_view.cache_template_loading = true.
-  config.cache_classes = true
 
-  # Eager loading loads your whole application. When running a single test locally,
-  # this probably isn't necessary. It's a good idea to do in a continuous integration
-  # system, or in some way before deploying your code.
-  config.eager_load = ENV['CI'].present?
+  # General application settings
+  config.cache_classes = true # Classes are cached (set to false under Spring and add config.action_view.cache_template_loading = true).
+  config.eager_load = ENV['CI'].present? # Eager load the whole application when running on a continuous integration system.
 
-  # Configure public file server for tests with Cache-Control for performance.
-  config.public_file_server.enabled = true
-  config.public_file_server.headers = {
-    'Cache-Control' => "public, max-age=#{1.hour.to_i}"
-  }
+  # Assets settings
+  config.assets.compile = true # Assets are compiled.
 
-  # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
-  config.cache_store = :null_store
+  # Caching settings
+  config.action_controller.perform_caching = false # Caching is disabled.
+  config.cache_store = :null_store # Use the null object as the cache store.
 
-  # Raise exceptions instead of rendering exception templates.
-  config.action_dispatch.show_exceptions = false
+  # File serving settings
+  config.public_file_server.enabled = true # Enable the public file server.
+  config.public_file_server.headers = { 'Cache-Control' => "public, max-age=#{1.hour.to_i}" } # Set Cache-Control for performance.
 
-  # Disable request forgery protection in test environment.
-  config.action_controller.allow_forgery_protection = true
+  # Error handling settings
+  config.consider_all_requests_local = true # Show full error reports.
+  config.action_dispatch.show_exceptions = false # Raise exceptions instead of rendering exception templates.
 
-  # Print deprecation notices to the stderr.
-  config.active_support.deprecation = :stderr
+  # Security settings
+  config.action_controller.allow_forgery_protection = true # Enable request forgery protection.
 
-  # Raise exceptions for disallowed deprecations.
-  config.active_support.disallowed_deprecation = :raise
+  # Active Support settings
+  config.active_support.deprecation = :stderr # Print deprecation notices to the stderr.
+  config.active_support.disallowed_deprecation = :raise # Raise exceptions for disallowed deprecations.
+  config.active_support.disallowed_deprecation_warnings = [] # Specify which deprecation messages to disallow.
 
-  # Tell Active Support which deprecation messages to disallow.
-  config.active_support.disallowed_deprecation_warnings = []
+  # I18n settings
+  # config.i18n.raise_on_missing_translations = true  # Uncomment to raise error for missing translations.
 
-  # Raises error for missing translations.
-  # config.i18n.raise_on_missing_translations = true
-
-  # Annotate rendered view with file names.
-  # config.action_view.annotate_rendered_view_with_filenames = true
+  # Action View settings
+  # config.action_view.annotate_rendered_view_with_filenames = true  # Uncomment to annotate rendered view with file names.
 end
