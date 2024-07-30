4aedcd6af0767128b3fd8dc6a6bd698f40d48ac6
Mostafa Sakr <msakr3@gmail.com>
2024-07-29 19:16:41 -0600

    Remove some text from home page and add text to enrollments index.

--- app/frontend/components/HomePage/HomePage.jsx
+++ app/frontend/components/HomePage/HomePage.jsx
@@ -6,7 +6,7 @@ function HomePage() {
     <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
       <div style={{ position: "absolute", top: "35%" }}>
       <p style={{ textAlign: "center" }}>Please choose a session at the library where you wish to take classes.</p>
-      <p style={{ textAlign: "center" }}>Each prospective student may register for only one enrollment session. Space is limited.</p>
+      <p style={{ textAlign: "center" }}>Each prospective student may register for only one enrollment session.</p>
       <p style={{ textAlign: "center" }}>Click <a href="/enrollments">here</a> to see a list of enrollment sessions to choose from.</p>
       </div>
     </div>
