03efd1c305c58a38ce975f409a8e19bb923bb136
Mostafa Sakr <msakr3@gmail.com>
2024-07-29 19:03:37 -0600

    Modify text in home page.

--- app/frontend/components/HomePage/HomePage.jsx
+++ app/frontend/components/HomePage/HomePage.jsx
@@ -4,9 +4,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function HomePage() {
   return (
     <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-      <div style={{ position: "absolute", top: "35%" }}>Please choose a session at the library where you wish to take classes.</div>
-      <div style={{ position: "absolute", top: "40%" }}>Each prospective student may register for only one session. Space is limited.</div>
-      <div style={{ position: "absolute", top: "50%" }}>Please click <a href="/enrollments">here</a> to see a list of enrollment sessions to choose from.</div>
+      <div style={{ position: "absolute", top: "35%" }}>
+      <p style={{ textAlign: "center" }}>Please choose a session at the library where you wish to take classes.</p>
+      <p style={{ textAlign: "center" }}>Each prospective student may register for only one enrollment session. Space is limited.</p>
+      <p style={{ textAlign: "center" }}>Click <a href="/enrollments">here</a> to see a list of enrollment sessions to choose from.</p>
+      </div>
     </div>
   );
 }
