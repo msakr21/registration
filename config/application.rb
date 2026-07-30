94e14db9fa5bfd2f2cc7baa64dfcf834ab2146cd
Mostafa Sakr <msakr3@gmail.com>
2026-07-30 16:45:42 -0600

    Add uk in application.rb

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module Registration
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
-    config.i18n.available_locales = [:en, :es, :ar, :ru]
+    config.i18n.available_locales = [:en, :es, :ar, :ru, :uk]
     config.i18n.default_locale = :en
 
     # Configuration for the application, engines, and railties goes here.
